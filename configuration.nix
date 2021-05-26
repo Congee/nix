1f91feec9d13f6777b260c89aa791230fdcf01b4
Congee <congeec@qq.com>
2021-05-26 14:58:04 -0400

    feat: firewall port 5900 for gnome-remote-deskop -> vncviewer

--- configuration.nix
+++ configuration.nix
@@ -123,11 +123,11 @@ in
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  networking.firewall.enable = true;
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 5900 ];  # vnc. why doesn't it work?
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
