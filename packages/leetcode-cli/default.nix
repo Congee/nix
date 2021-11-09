0fbc585956d389f456ee7001ffb67962e88a390b
Congee <congeec@qq.com>
2021-11-09 17:36:28 -0500

    fix(pkgs): bump leetcode-cli from 0.3.5 to 0.3.9

--- packages/leetcode-cli/default.nix
+++ packages/leetcode-cli/default.nix
@@ -1,24 +1,37 @@
-with import <nixpkgs> {};
+with import <nixpkgs> {
+  overlays = [
+    (import (builtins.fetchGit {
+      url = https://github.com/oxalica/rust-overlay;
+      rev = "35dd4b5611a1a7bec74e1a837e38d2a8b72909ba";
+    }))
+  ];
+};
 
-rustPlatform.buildRustPackage rec{
+let
+  platform = makeRustPlatform {
+    rustc = rust-bin.nightly.latest.minimal;
+    cargo = rust-bin.nightly.latest.minimal;
+  };
+in platform.buildRustPackage rec{
   pname = "leetcode-cli";
-  version = "0.3.5";
+  version = "0.3.9";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "09zf17awssrb8kr4ag5lbisng2abx7yazmaaxvf5j2l4pafivgfb";
+    sha256 = "1aiksg4iyrhkmwl4djy7bm3xjras4qqfixi6ml8k6pz0s9ynknws";
   };
 
-  cargoSha256 = "1mwvyb4f48y5nixddl5zh32s5d77zxsd4864db6lbjpdk0vfrsfr";
+  cargoSha256 = "1ldzr2bspy288nydk2lnkc4akc2myk2jk7aw87vr4kmzl0jmh329";
   
   # a nightly compiler is required unless we use this cheat code.
-  RUSTC_BOOTSTRAP = 1;
+  RUSTC_BOOTSTRAP = 0;
 
-  CFG_RELEASE = "${rustPlatform.rust.rustc.version}-nightly";
+  # CFG_RELEASE = "${rustPlatform.rust.rustc.version}-nightly";
   CFG_RELEASE_CHANNEL = "ngihtly";
 
   nativeBuildInputs = [
     pkg-config
+    rust-bin.nightly."2021-05-16".minimal
   ];
 
   buildInputs = [
