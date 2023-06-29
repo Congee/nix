822eee641e6328f7d0e5d763918e3cab40da3649
Congee <congeec@qq.com>
2023-06-29 15:07:39 -0400

    feat(blackbox): services.tailscale.enable = true;

--- hosts/blackbox/configuration.nix
+++ hosts/blackbox/configuration.nix
@@ -57,6 +57,8 @@ in
   };
   networking.dhcpcd.enable = false;
 
+  services.tailscale.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
