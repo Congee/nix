a444ea03dfe7a9feef8e3e8e6908bd39ddf568e2
Congee <congeec@qq.com>
2024-12-03 20:03:44 -0500

    feat(nix): add nix.settings.trusted-users

--- hosts/nixcmd.nix
+++ hosts/nixcmd.nix
@@ -2,8 +2,9 @@
 
 {
   nix = {
+    settings.trusted-users = [ "@admin" "@wheel" ];
     extraOptions = ''
-      experimental-features = nix-command flakes repl-flake
+      experimental-features = nix-command flakes
       keep-outputs = true
       keep-derivations = true
     '';
@@ -13,10 +14,12 @@
     settings.trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     settings.substituters = [
       "https://cache.iog.io/"
       "https://nixpkgs-wayland.cachix.org"
+      "https://nix-community.cachix.org/"
     ];
   };
 }
