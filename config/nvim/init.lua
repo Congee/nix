c6aca2992ce036ff9d5e8ea75163edf4a099f3e9
Congee <congeec@qq.com>
2026-02-16 14:36:50 -0500

    feat(nvim): disable some rtp plugins for performance

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,4 +15,24 @@ local __file__ = debug.getinfo(1).short_src
 local vimrc = __file__:match("@?(.*/)") .. 'vimrc'
 vim.api.nvim_command('source ' .. vimrc)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = { notify = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'netrwPlugin',
+        'rplugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    }
+  }
+})
+
+-- https://www.reddit.com/r/neovim/comments/1kcz8un/great_improvements_to_the_cmdline_in_nightly/
+require('vim._core.ui2').enable({})
+
+vim.cmd.packadd 'nvim.undotree'
